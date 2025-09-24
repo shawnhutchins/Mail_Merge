f9def971672d87eae9877461a294c70290e78d06
shawn <shawnhutchins1@gmail.com>
2025-09-23 21:45:54 -0400

    Changed reading in the letter to use read() instead of readlines()

--- main.py
+++ main.py
@@ -9,16 +9,14 @@ with open("./Input/Names/invited_names.txt") as file:
 
 #reading in the lines from starting_letter.txt into the letter variable
 with open("./Input/Letters/starting_letter.txt") as file:
-    letter = file.readlines()
+    letter = file.read()
 
 #Replace the [name] placeholder with the actual name.
 for name in names:
-    new_letter = []
-    for line in letter:
-        new_letter.append(line.replace(PLACEHOLDER, name))
+    new_letter = letter.replace(PLACEHOLDER, name)
     #write the new letter to a file in the "ReadyToSend" folder
     with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
-        file.writelines(new_letter)
+        file.write(new_letter)
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
