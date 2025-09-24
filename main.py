262f7bd002c447e43afda83fee2d72c127c8d0b2
shawn <shawnhutchins1@gmail.com>
2025-09-23 21:49:55 -0400

    Removed hints

--- main.py
+++ main.py
@@ -17,7 +17,3 @@ for name in names:
     #write the new letter to a file in the "ReadyToSend" folder
     with open(f"./Output/ReadyToSend/letter_for_{name}.txt", mode="w") as file:
         file.write(new_letter)
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
